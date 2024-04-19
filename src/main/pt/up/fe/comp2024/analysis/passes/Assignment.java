3e8bf7e5c3a463a6945bcde7b0c2db2593cc8689
Sofia Moura <sofia.smoura@hotmail.com>
2024-04-19 18:35:35 +0100

    no

--- src/main/pt/up/fe/comp2024/analysis/passes/Assignment.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/Assignment.java
@@ -36,16 +36,6 @@ public class Assignment extends AnalysisVisitor {
             return null;
         }
 
-        if(left.get("value").equals("this")) {
-            addReport(Report.newError(
-                    Stage.SEMANTIC,
-                    NodeUtils.getLine(assignment),
-                    NodeUtils.getColumn(assignment),
-                    "Left Var is not a variable!",
-                    null)
-            );
-            return null;
-        }
 
         if(left.getKind().equals("IntegerLiteral")) {
             addReport(Report.newError(
